203032fd2452e0a09a640d4db395fc02d192340a
hoonyland <hoonyland.newsletter@gmail.com>
2024-02-14 19:43:33 +0900

    update: from beautifulfund

--- src/app/welcome/[eventSlug]/page.tsx
+++ src/app/welcome/[eventSlug]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { createClient } from "@supabase/supabase-js";
-import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -20,7 +19,7 @@ export default function Home() {
 
   const refresh = async () => {
     const { data } = await supabase
-      .from(TABLE_NAME)
+      .from(process.env.NEXT_PUBLIC_TABLE_NAME as string)
       .select("*")
       .eq("event_slug", params.eventSlug);
 
@@ -60,22 +59,18 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/logo.gif"
-          alt="Sluggish Hackers"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="grid text-center lg:max-w-5xl lg:w-full lg:text-left">
+      <div className="grid text-center lg:max-w-5xl lg:w-full lg:text-left mt-20">
         <p className="text-2xl font-bold">
-          {participants
-            .map((p) => `${p.org ? `${p.org} - ` : ""}${p.name}`)
-            .join(", ")}
+          {participants.map((p, index) => (
+            <span
+              className="text-slate-200 ml-4 bg-black italic first-of-type:ml-0"
+              key={index}
+            >
+              {p.org ? `${p.org} - ` : ""}
+              {p.name}
+              {index < participants.length - 1 ? ", " : ""}
+            </span>
+          ))}
         </p>
       </div>
     </main>
